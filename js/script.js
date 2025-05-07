061558497c107220f0ffacfbc69a429e1386c810
Domenic <domenic.dangelo@stu.ocsb.ca>
2025-05-07 00:27:40 +0000

    natural numbers without favicons

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 domeic dangelo All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: domenic dangelo
+// Created on: feb 2025
 // This file contains the JS functions for index.html
+
+'use strict'
+
+/**
+ * This function checks if the user has selected the random number from above.
+ */
+// eslint-disable-next-line no-unused-vars
+function calculate() {
+  // process
+  let nValue = parseFloat(document.getElementById("nValue").value);
+  let text = ""
+  let number = 0;
+
+  // input
+  for (let naturalNumber = 1; naturalNumber <= nValue; naturalNumber++) {
+    number += naturalNumber;
+  }
+  // output
+  text += "The sum of natural numbers is " + number + "<br>";
+  document.getElementById("result").innerHTML = text;
+} 
